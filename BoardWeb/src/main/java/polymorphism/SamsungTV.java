7a6bbe92c712c6ddcbeb3456984894a74f67946d
DaeJin Jeong <devwithu@naver.com>
2018-02-11 02:01:37 +0900

    Class 03, 스프링컨테이너 및 설정파일

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,7 +1,15 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
-
+	
+	public void initMethod(){
+		System.out.println("====> SamsungTV() 객체초기화");
+	}
+	
+	public void destroyMethod(){
+		System.out.println("====> SamsungTV() 객체소멸");
+	}
+	
 	public void powerOn() {
 		System.out.println("SamsungTV --전원켜다.");
 	}
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,17 +1,22 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
 		
-		//TV tv = new LgTV();
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
 		
-		BeanFactory beanFactory = new BeanFactory();
-		TV tv = (TV)beanFactory.getBean(args[0]);
+		TV tv=(TV)factory.getBean("tv");
 		tv.powerOn();
+		tv.powerOff();
 		tv.volumeUp();
 		tv.volumeDown();
-		tv.powerOff();
+		
+		factory.close();
 		
 	}
 }
